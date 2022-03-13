5699bbe4fc5147b34cc367a2d58f6637064820d7
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-03-13 21:26:22 +0300

    Modify 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main: main function
  * Description: Print out numbers from 0 to 10, then followed by newline
  * Return: 0
  */
 int main(void)
-{	int i = 0;
+{	
+	int i = 0;
 
 	for (i = 0; i < 10; i++)
 		printf("%d", i);
