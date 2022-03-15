1c7048fe1781aa6dcce7c7cc0cf88b28fd292887
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-03-15 22:38:45 +0300

    Add 5-sign.c and 5-main, Modify main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of the number
+ * @n: The number paramater
+ * Return: 0, 1 , or -1 based on the sign
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	_putchar('-');
+	return (0);
+}
