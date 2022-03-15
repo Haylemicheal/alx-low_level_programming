be5802d48349d105fc42528e71808f9a0e1878a0
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-03-15 23:00:05 +0300

    Add 7-main.c and 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.c"
+
+/**
+ * print_last_digit - Prints the last digit of the number
+ * @n: Number param
+ * Return: The last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
