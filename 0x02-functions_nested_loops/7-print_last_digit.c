d6423ba2b8a961eacc2b338a801a05f8d6b3d8d6
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-03-15 23:42:45 +0300

    Fix error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int i;
+
+	i = n >= 0 ? n : (n + 1) * -1;
+	if (n < 0)
+	{
+		_putchar(49 + (i % 10));
+		return ((i % 10) + 1);
+	}
+	_putchar(48 + i % 10);
+	return (i % 10);
 }
