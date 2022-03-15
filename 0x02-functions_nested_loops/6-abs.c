e6064c34b62ca98b228477cd2d0f6161ca487df3
Haylemicheal Berihun <haylemicheal.berihun@dventus.com>
2022-03-15 22:51:08 +0300

    Add 6-main.c and 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absoulte value of an integer
+ * @n: Number param
+ * Return: The absoulte value of the input
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
+
