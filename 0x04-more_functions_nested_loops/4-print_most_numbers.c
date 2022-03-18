edfe2edbb2ac9205c3c15f19386ce02e47be5792
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-18 21:30:40 +0300

    Fix spacing issue

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,17 @@
  */
 void print_most_numbers(void)
 {
-        int i = 48;
+	int i = 48;
 
-        while (i <= 57)
-        {
+	while (i <= 57)
+	{
 		if (i == 50 || i == 52)
 		{
 			i++;
 			continue;
 		}
-                _putchar(i);
-                i++;
-        }
-        _putchar('\n');
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
 }
