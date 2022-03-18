45dbe7cc4eb3883e14e02017f61ab13451955bbf
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-18 22:55:55 +0300

    Fix Typo

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j< i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(32);
 		}
 		_putchar(92);
-		_putchar(36);
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
