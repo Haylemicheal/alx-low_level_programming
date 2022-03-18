fc4b3629c0550c82c3ca41911f8f5f12349d8c3f
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-18 22:40:43 +0300

    Add 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print \$ in diagonal
+ * @n: height of the print characters
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j< i; j++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar(36);
+		_putchar('\n');
+	}
+}
