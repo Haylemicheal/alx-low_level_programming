6a4fb9eb6c452d4bae7aac9c293ef7c315b58941
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-18 19:42:20 +0300

    Add _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 for uppercase letters or 0 for lower case letters
+ * @c: int param
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 91)
+	{
+		return (1);
+	}
+	return (0);
+}
