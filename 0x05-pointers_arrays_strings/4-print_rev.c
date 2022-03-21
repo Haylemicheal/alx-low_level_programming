4dad1ffe6d3a19f05b7346397eba592ff41d595a
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-21 22:42:03 +0300

    Add print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: String input
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}
