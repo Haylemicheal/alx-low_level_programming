fa3fbb4ac54499625071ad2fad60518fd20f12e0
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-21 23:09:55 +0300

    Fix trailing ehitespace issue

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string: reverses a string
+ * rev_string - reverses a string
  * @s: String input
  * Return: none
  */
@@ -23,5 +23,6 @@ void rev_string(char *s)
 		s[len - i - 1] = s[i];
 		s[i] = temp;
 		i++;
-	}	
+	}
 }
+
