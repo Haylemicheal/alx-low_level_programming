b0a6dc79f141925746b2b63666461e21200037da
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-24 03:59:48 +0300

    Fix increament issue

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,12 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; *dest != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+	for (j = 0; src[j] != '\0'; j++)
 	{
+		dest[i] = src[j];
 		i++;
-		dest++;
-	}
-	for (j = 0; *src != '\0'; j++)
-	{
-		dest[i] = *src;
-		i++;
-		j++;
-		src++;
 	}
 	return (dest);
 }
