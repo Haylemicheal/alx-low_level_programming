2e48743edd321ab64c9c0647d8e20fee5757d1af
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-28 21:17:51 +0300

    Add _strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in string
+ * @s: String input
+ * @c: Char input
+ * Return: The occurance of the char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
