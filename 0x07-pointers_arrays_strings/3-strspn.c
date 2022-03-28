d3a71e146e60e5037f5705355d7023491fbdd14f
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-28 22:42:01 +0300

    Add strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - get length of a prefix substring
+ * @s: String input
+ * @accept: String input
+ * Return: The number of bytes 
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
