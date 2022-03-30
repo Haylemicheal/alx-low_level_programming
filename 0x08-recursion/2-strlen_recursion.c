4d6bf932777698a6b607567d7b6b5910518560ef
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-03-30 21:27:56 +0300

    Add strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of the string
+ * @s: String input
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
