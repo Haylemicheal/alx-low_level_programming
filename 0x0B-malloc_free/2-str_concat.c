1e4a0be7c57c6527ad368ca221310e57ee9f0c6d
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-04-05 21:45:12 +0300

    Add str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatnates two strings
+ * @s1: string input 1
+ * @s2: string input 2
+ * Return: s1 + s2 or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int s1_len = 0;
+	unsigned int s2_len = 0;
+	unsigned int i, j;
+	char *concat;
+
+	if (s1 == NULL && s2 != NULL)
+	{
+		s2 = "";
+	}
+	else if (s2 == NULL && s1 != NULL)
+	{
+		s1 = "";
+	}
+
+	while (*(s1 + s1_len) != '\0')
+	{
+		s1_len++;
+	}
+
+	while (*(s2 + s2_len) != '\0')
+	{
+		s2_len++;
+	}
+
+	concat = malloc(sizeof(*s1) * s1_len + sizeof(*s2) * s2_len + 1);
+
+	if (concat == NULL)
+		return ('\0');
+
+	for (i = 0; i < s1_len; i++)
+		concat[i] = *(s1 + i);
+
+	for (j = 0; j < s2_len; j++, i++)
+		concat[i] = *(s2 + j);
+
+	return (concat);
+}
