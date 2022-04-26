868fc55683e523e0f355e103b48563e2f9fc556b
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-04-26 20:14:19 +0300

    Fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,10 +11,10 @@ int pop_listint(listint_t **head)
 	listint_t *node = *head;
 	int n;
 
-	if (*head != NULL)
+	if (*head == NULL)
 		return (0);
 	n = node->n;
-	*head = (*head)->next;
+	*head = node->next;
 	free(node);
 	return (n);
 }
