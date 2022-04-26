49a249e4bd6a0493382fc9676d848803887d977f
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-04-26 20:28:38 +0300

    Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * insert_nodeint_at_index - Insert node at index idx
@@ -9,21 +10,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node = create_node(n);
-	listint_t *current_node = *head;
-	unsigned int i = 0;
+unsigned int i = 0;
+	listint_t *current_node = *head, *new_node_ptr;
 
-	while (current_node && (i < idx - 1))
+	if (idx == 0)
 	{
-		current_node = (current_node)->next;
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = *head;
+		*head = new_node_ptr;
+		return (new_node_ptr);
+	}
+
+	while (current_node && ((i + 1) != idx))
+	{
+		current_node = current_node->next;
 		i++;
 	}
 
-	if (i < idx - 1)
-		return (NULL);
-	node->next = (current_node)->next;
-	(current_node)->next = node;
+	if ((i + 1) == idx)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
 
-	return (*head);
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = current_node->next;
+		current_node->next = new_node_ptr;
+		return (new_node_ptr);
+	}
+	return (NULL);
 }
 
