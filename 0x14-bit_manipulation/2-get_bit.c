390edbcbdd68bfc97e2dfcf54c5bebfa9094aaaf
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-05-05 11:55:24 +0300

    Add get_bit at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - Get bit at specified index
+ * @n: integer input
+ * @index: index
+ * Return: 1 or -1 or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	
+	return ((n >> index) & 1);
+}
