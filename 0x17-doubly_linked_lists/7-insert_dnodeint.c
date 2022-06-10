f2ed0c1fbb418302c653fb19651bb8acaed552b1
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-06-10 16:58:56 +0300

    Fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,6 +40,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->next->prev = node;
 	temp->next = node;
 	node->prev = temp;
-	
-	return (*h);
+
+	return (node);
 }
